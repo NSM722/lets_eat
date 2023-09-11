b7aeb59af4b36a7068f45b5e54d7122a74ff01fa
Sakina Mbatha <sakina@natix.io>
2023-09-11 16:46:09 +0200

    feat: make the widgets tappable

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -11,24 +11,30 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.45),
-            category.color.withOpacity(0.85),
-          ],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor, // for tapping effect
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.45),
+              category.color.withOpacity(0.85),
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontSize: 16,
-            ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontSize: 16,
+              ),
+        ),
       ),
     );
   }
