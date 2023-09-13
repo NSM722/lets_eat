2819574ee44bf23780bc4dfc15589685ad0bda38
Sakina Mbatha <sakina@natix.io>
2023-09-13 11:45:40 +0200

    refactor: deleted repetitive appBar title

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -26,28 +26,23 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Select a category'),
+    return GridView(
+      padding: const EdgeInsets.all(22),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // set the number of columns = 2
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(22),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // set the number of columns = 2
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          for (final category in availableCategories)
-            CategoryGridItem(
-              onSelectCategory: () {
-                _selectCategory(context, category);
-              },
-              category: category,
-            ),
-        ],
-      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+            category: category,
+          ),
+      ],
     );
   }
 }
