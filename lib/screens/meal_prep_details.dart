d796aee67826b5c1ac2dd7eb0e7145e5f8ae3fbf
Sakina Mbatha <sakina@natix.io>
2023-09-25 13:42:59 +0200

    feat: configure implicit animation

--- lib/screens/meal_prep_details.dart
+++ lib/screens/meal_prep_details.dart
@@ -20,19 +20,18 @@ class MealPrepDetailsScreen extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          meal.title,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // trigger the notifier method
-              final wasMealAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleFavoriteMeal(meal); // use read() not watch()
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
+          title: Text(
+            meal.title,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // trigger the notifier method
+                final wasMealAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleFavoriteMeal(meal); // use read() not watch()
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                   content: Text(
                     wasMealAdded
                         ? 'Meal marked as favorite'
@@ -41,85 +40,76 @@ class MealPrepDetailsScreen extends ConsumerWidget {
                   backgroundColor: Colors.lightGreenAccent,
                   closeIconColor: const Color.fromARGB(255, 28, 30, 16),
                   showCloseIcon: true,
-                ),
-              );
-            },
-            // animating the icon
-            icon: AnimatedSwitcher(
-              duration: const Duration(
-                milliseconds: 250,
-              ),
-              transitionBuilder: (child, animation) {
-                return RotationTransition(
-                  turns: animation,
-                  child: child,
-                );
+                ));
               },
-              child: Icon(
-                isFavorite ? Icons.star : Icons.star_border,
+              // animating the icon
+              icon: AnimatedSwitcher(
+                duration: const Duration(
+                  milliseconds: 250,
+                ),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: animation,
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  key: ValueKey(
+                    isFavorite,
+                  ),
+                  isFavorite ? Icons.star : Icons.star_border,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+            )
+          ]),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity, // occupies all available space
-              height: 270,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            Text(
-              'Ingredients',
+        child: Column(children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity, // occupies all available space
+            height: 270,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Text('Ingredients',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
                     fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            for (final ingredient in meal.ingredients)
-              Text(
-                ingredient,
+                  )),
+          const SizedBox(
+            height: 14,
+          ),
+          for (final ingredient in meal.ingredients)
+            Text(ingredient,
                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                       color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-            const SizedBox(
-              height: 24,
-            ),
-            Text(
-              'Steps',
+                    )),
+          const SizedBox(
+            height: 24,
+          ),
+          Text('Steps',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
                     fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            for (final step in meal.steps)
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 8,
-                ),
-                child: Text(
-                  step,
+                  )),
+          const SizedBox(
+            height: 14,
+          ),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 8,
+              ),
+              child: Text(step,
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                         color: Theme.of(context).colorScheme.onBackground,
-                      ),
-                ),
-              ),
-          ],
-        ),
+                      )),
+            ),
+        ]),
       ),
     );
   }
